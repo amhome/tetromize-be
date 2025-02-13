c91f1cf212b512e4bc1e8add9a889649621d5294
amhome <amirhosseinmehrvarzi@gmail.com>
2025-02-13 16:36:13 -0500

    + algorithm

--- flight/Program.cs
+++ flight/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using Microsoft.EntityFrameworkCore;
+using Tetromize.Data;
+
+DateTime fromDate = new DateTime(2018, 1, 1);
+DateTime toDate = new DateTime(2018, 1, 15);
+int agencyId = 1;
+
+// algorithm
+using var context = new AppDbContext();
+
+var _subscriptions = await context.Subscriptions.Where(x => x.AgencyId == agencyId).ToListAsync();
+
+var routesQuery = context.Routes.Where(x => x.DepartureDate > fromDate && x.DepartureDate < toDate).AsQueryable();
+routesQuery = routesQuery.Where(x => _subscriptions.Any(s => s.OriginCityId == x.OriginCityId && s.DestinationCityId == x.DestinationCityId));
+var _routes = await routesQuery.Include(x => x.Flights).ToListAsync();
+
+foreach (var group in _routes.SelectMany(x => x.Flights).GroupBy(x => x.AirlineId))
+{
+    var newOnes = group.Where(x => 
+        !group.Any(y => x.DepartureTime >= y.DepartureTime.AddDays(-7).AddMinutes(-30) && x.DepartureTime <= y.DepartureTime.AddDays(-7).AddMinutes(30)));
+    var discontinued = group.Where(x => 
+        !group.Any(y => x.DepartureTime >= y.DepartureTime.AddDays(7).AddMinutes(-30) && x.DepartureTime <= y.DepartureTime.AddDays(7).AddMinutes(-30)));
+}
